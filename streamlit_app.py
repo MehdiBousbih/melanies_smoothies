8b7878bdf1d5d297d930c6bfe11f729805536831
MehdiBousbih <118083449+MehdiBousbih@users.noreply.github.com>
2024-03-22 17:11:53 +0100

    Update streamlit_app.py
    fix response

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,9 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect('Choose up to 4 ingedients',
                                 my_dataframe,
                                 max_selections = 5)
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
 if ingredients_list:
 
